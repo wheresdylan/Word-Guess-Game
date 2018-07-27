b42bd1d233b968363857c4b57ff62a655d2c59aa
Dylan Marini <dylan.marini@g.austincc.edu>
2018-07-27 12:33:45 -0500

    final product

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,31 +2,31 @@ var footballTeams = ["PATRIOTS", "COWBOYS", "RAMS", "DOLPHINS", "PANTHERS", "BIL
 var userGuess;
 var wins = 0;
 var losses = 0;
-var remainingGuess = 12;
+var remainingGuess = 8;
 var currentWord;
 var emptyWord = ""
-var patriots = new Audio("WeAreTheChampions.mp3")
-var cowboys = new Audio("cowboys SUCK!.mp3")
-var rams = new Audio("Mic Nice - Rams House (Whose House).mp3")
-var dolphins = new Audio("Miami dolphins fight song.mp3")
-var panthers = new Audio("MARK B. - CAROLINA ANTHEM (SHOT BY @DJCHUCKT).mp3")
-var bills = new Audio("Buffalo Bills Shout Song.mp3")
+var patriots = new Audio("assets/Audio/WeAreTheChampions.mp3")
+var cowboys = new Audio("assets/Audio/cowboys SUCK!.mp3")
+var rams = new Audio("assets/Audio/Mic Nice - Rams House (Whose House).mp3")
+var dolphins = new Audio("assets/Audio/Miami dolphins fight song.mp3")
+var panthers = new Audio("assets/Audio/MARK B. - CAROLINA ANTHEM (SHOT BY @DJCHUCKT).mp3")
+var bills = new Audio("assets/Audio/Buffalo Bills Shout Song.mp3")
 
+//creating word, empty word, and letters guessed array
 currentWord = footballTeams[Math.floor(Math.random() * footballTeams.length)];
-
 
 for (i = currentWord.length; i > 0; i--){
     emptyWord += "-";
 }
 
-
 var lettersGuessed = [];
 
-
+//function that replaces a character at a given string, index, and word
 function replaceAt(string, index, replace) {
     return string.substring(0, index) + replace + string.substring(index + 1);
   }
 
+//function that resets game after loss and adds 1 loss
 function lose(){
 
     emptyWord = "";
@@ -41,9 +41,10 @@ function lose(){
 
     lettersGuessed = [];
 
-    remainingGuess = 12;
+    remainingGuess = 8;
 }
 
+//function that increases win count and resets game
 function win(){
 
     changePicture();
@@ -60,10 +61,11 @@ function win(){
 
     lettersGuessed = [];
 
-    remainingGuess = 12;
+    remainingGuess = 8;
 
 }
 
+//function that decreases the guess count if user guess is not guessed already
 function decreaseGuess(){
     if(emptyWord.includes(userGuess) || lettersGuessed.includes(userGuess) || userGuess === "ENTER"){
         
@@ -73,6 +75,7 @@ function decreaseGuess(){
     }
 }
 
+//function that adds letters to letters guessed with user input
 function letters(){
     if(emptyWord.includes(userGuess) || lettersGuessed.includes(userGuess) || userGuess === "ENTER"){
         
@@ -82,24 +85,23 @@ function letters(){
     }
 }
 
+//function that replaces the empty word with user guess (if character exists)
 function replaceWord(){
     for (r = 0; r < currentWord.length; r++){
-        
         if(currentWord[r] === userGuess){
            emptyWord = replaceAt(emptyWord, r, userGuess);
         }
    }
 }
 
+//function that changes the picture and plays the audio for that specific picture
 function changePicture(){
 
     if(emptyWord === "PATRIOTS"){
         document.getElementById("image").src="https://images-na.ssl-images-amazon.com/images/I/71ThCKlLrhL._SL1500_.jpg";
 
-
         stopAudio();
         patriots.play();
-
     }
 
     if(emptyWord === "COWBOYS"){
@@ -107,8 +109,6 @@ function changePicture(){
 
         stopAudio();
         cowboys.play();
-
-
     }
 
     if(emptyWord === "RAMS"){
@@ -116,8 +116,6 @@ function changePicture(){
 
         stopAudio();
         rams.play();
-
-
     }
 
     if(emptyWord === "DOLPHINS"){
@@ -125,8 +123,6 @@ function changePicture(){
 
         stopAudio();
         dolphins.play();
-
-
     }
 
     if(emptyWord === "PANTHERS"){
@@ -134,8 +130,6 @@ function changePicture(){
 
         stopAudio();
         panthers.play();
-
-
     }
 
     if(emptyWord === "BILLS"){
@@ -143,12 +137,10 @@ function changePicture(){
 
         stopAudio();
         bills.play();
-
-
     }
-
 }
 
+//function that stops audio and restarts it
 function stopAudio(){
     patriots.pause();
     patriots.currentTime = 0;
@@ -169,33 +161,34 @@ function stopAudio(){
     dolphins.currentTime = 0;
 }
 
-
-
+//main function. activates after user touches a key
 document.onkeyup = function(event){
 
     userGuess = event.key.toUpperCase();
 
+    //calls to functions
     replaceWord();
     decreaseGuess();
     letters();
 
-
-     document.getElementById("current_word").firstChild.data = emptyWord;
+    document.getElementById("current_word").firstChild.data = emptyWord;
 
     if (footballTeams.includes(emptyWord)){
-       win();
+        win();
     }
 
     if (remainingGuess === 0){
+        document.getElementById("current_word").firstChild.data = currentWord;
         lose();
     }
-
+    
+    //replaces the current id's with the new ones
     document.getElementById("number_wins").firstChild.data = wins;
     document.getElementById("remaining_guess").firstChild.data = remainingGuess;
     document.getElementById("taken_letters").firstChild.data = lettersGuessed;
     document.getElementById("number_losses").firstChild.data = losses;
     
-
+    //console.log all of the answers
     console.log("button pressed " + userGuess);
     console.log("the current word is " + currentWord);
     console.log("the empty word is " + emptyWord);
@@ -203,7 +196,7 @@ document.onkeyup = function(event){
     console.log("letters guessed " + lettersGuessed);
     console.log("number of wins " + wins);
     console.log("number of losses " + losses); 
-
+    
 }
 
 
